77d3c0d2bfe4fc69da17698ac30f7b31fa3a048f
Kevin Wei <chenghaowei@gmail.com>
2019-11-08 16:31:22 -0500

    add dead code back for nancy

--- queue_Stack.js
+++ queue_Stack.js
@@ -4,10 +4,20 @@ class Queue {
   constructor() {
     this.stack1 = new Stack();
     this.stack2 = new Stack();
+    // this.first = null;
+    // this.last = null;
   }
 
   enqueue(data) {
-    this.stack2.push(data);
+    if (this.stack2.top === null) {
+      this.stack2.push(data);
+      // this.first = this.stack2.top;
+      // this.last = this.first;
+    } else {
+      this.stack2.push(data);
+      // this.last = this.stack2.top;
+    }
+    // console.log('stack2', this.stack2);
   }
 
   dequeue() {
@@ -16,11 +26,21 @@ class Queue {
         this.stack1.push(this.stack2.pop());
       }
     }
-    return this.stack1.pop();
+    let temp = this.stack1.pop();
+    if (this.stack1.top === null) {
+      // this.first = null;
+      // this.last = null;
+    }
+    // console.log('stack1', this.stack1);
+    return temp;
   }
 }
 
 function peek(queue) {
+  if (queue.first) {
+    return queue.first.data;
+  }
+  return null;
 }
 
 function isEmpty(stack) {
@@ -28,6 +48,14 @@ function isEmpty(stack) {
 }
 
 function display(queue) {
+  // let result = '';
+  // let curr = queue.first;
+  // while (curr !== null) {
+  //   result += curr.data + ', ';
+  //   curr = curr.next;
+  // }
+  // return result;
+
 }
 
 function main() {
@@ -40,7 +68,6 @@ function main() {
   q.enqueue('Chewbacca');
   q.dequeue();
   q.enqueue('HanSolo');
-  q.dequeue();
   q.dequeue();
   console.log(JSON.stringify(q, null, 2));
 }
